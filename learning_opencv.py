b0fb2dc9aa33dcba190d9eec38a7e0a0b95136aa
Phong Tran <s2@puyonexus.com>
2019-01-02 06:43:41 -0500

    searching for avgs

---
+++ learning_opencv.py
@@ -0,0 +1,116 @@
+import numpy as np
+import argparse
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument('-i', '--image', required = True, help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args['image'])
+# print('width: {} pixels'.format(image.shape[1]))
+# print('height: {} pixels'.format(image.shape[0]))
+# print('channels: {}'.format(image.shape[2]))
+
+# Find regions
+# Player 1 Board
+cv2.rectangle(image, (279, 160), (663, 880), (255, 0, 0), 1)
+# Player 2 Board
+cv2.rectangle(image, (1256, 160), (1640, 880), (0, 0, 255), 1)
+
+# cv2.imshow('Image', image)
+# cv2.waitKey(0)
+
+# Get average color of board unit?
+image = cv2.imread(args['image'])
+# Crop P1, row 1, col 1
+# https://stackoverflow.com/questions/43111029/how-to-find-the-average-colour-of-an-image-in-python-with-opencv
+crop_puyo = image[820:880, 279:343]
+average = np.average(np.average(crop_puyo, axis=0), axis=0) # Outputs BGR
+
+def cropPuyo(input_img, player_num, rowcoltuple):
+    row = rowcoltuple[0]
+    col = rowcoltuple[1]
+
+    start_x, start_y = 0, 0
+
+    if player_num == 1:
+        start_x = 279
+        start_y = 160
+    elif player_num == 2:
+        start_x = 1256
+        start_y = 160
+
+    x1 = start_x + 64 * (col - 1)
+    x2 = start_x + 64 * (col)
+    y1 = start_y + 60 * (12 - row)
+    y2 = start_y + 60 * (12 - row + 1)
+
+    return input_img[y1:y2, x1:x2]
+
+
+# Compile averages
+# List out row,col positions
+blue_puyos = [(1, 1), (1, 2), (1, 3), (1, 5), (1, 6),
+              (2, 4), (3, 2), (3, 3), (3, 5), (3, 6),
+              (4, 3), (4, 6)]
+blue_BGR_data = []
+for pos in blue_puyos:
+    row = pos[0]
+    col = pos[1]
+    puyo = cropPuyo(image, 1, (row, col))
+    blue_BGR_data.append(np.average(np.average(puyo, axis=0), axis=0))
+blue_avg_triplet = np.average(np.array(blue_BGR_data), axis=0)
+blue_stdev_triplet = np.std(np.array(blue_BGR_data), axis=0)
+print('Blue:')
+print(blue_avg_triplet)
+print(blue_stdev_triplet)
+print('\n\n')
+
+red_puyos = [(3, 1), (4, 1), (4, 2), (4, 4), (5, 3), (7, 1), (8, 1)]
+red_BGR_data = []
+for pos in red_puyos:
+    row = pos[0]
+    col = pos[1]
+    puyo = cropPuyo(image, 1, (row, col))
+    red_BGR_data.append(np.average(np.average(puyo, axis=0), axis=0))
+red_avg_triplet = np.average(np.array(red_BGR_data), axis=0)
+red_stdev_triplet = np.std(np.array(red_BGR_data), axis=0)
+print('Red:')
+print(red_avg_triplet)
+print(red_stdev_triplet)
+print('\n\n')
+
+green_puyos = [(2, 1), (2, 2), (2, 3), (5, 6), (6, 4), (6, 5)]
+green_BGR_data = []
+for pos in green_puyos:
+    row = pos[0]
+    col = pos[1]
+    puyo = cropPuyo(image, 1, (row, col))
+    green_BGR_data.append(np.average(np.average(puyo, axis=0), axis=0))
+green_avg_triplet = np.average(np.array(green_BGR_data), axis=0)
+green_stdev_triplet = np.std(np.array(green_BGR_data), axis=0)
+print('green:')
+print(green_avg_triplet)
+print(green_stdev_triplet)
+print('\n\n')
+
+purple_puyos = [(1, 4), (2, 5), (2, 6), (3, 4), (4, 5), (5, 1), (5, 2), (5, 4), (5, 5),
+                (6, 1), (6, 3), (7, 4), (7, 5)]
+purple_BGR_data = []
+for pos in purple_puyos:
+    row = pos[0]
+    col = pos[1]
+    puyo = cropPuyo(image, 1, (row, col))
+    purple_BGR_data.append(np.average(np.average(puyo, axis=0), axis=0))
+purple_avg_triplet = np.average(np.array(purple_BGR_data), axis=0)
+purple_stdev_triplet = np.std(np.array(purple_BGR_data), axis=0)
+print('purple:')
+print(purple_avg_triplet)
+print(purple_stdev_triplet)
+print('\n\n')
+
+
+# cv2.imshow('Single Puyo', crop_puyo)
+# cv2.imwrite('blue_puyo.jpg', crop_puyo)
+# cv2.waitKey(0)
+
---
+++ puyo_calibration.py
@@ -0,0 +1,128 @@
+import numpy as np
+import cv2
+
+def cropPuyo(input_img, player_num, rowcoltuple):
+    row = rowcoltuple[0]
+    col = rowcoltuple[1]
+
+    start_x, start_y = 0, 0
+
+    if player_num == 1:
+        start_x = 279
+        start_y = 160
+    elif player_num == 2:
+        start_x = 1256
+        start_y = 160
+
+    x1 = start_x + 64 * (col - 1)
+    x2 = start_x + 64 * (col)
+    y1 = start_y + 60 * (12 - row)
+    y2 = start_y + 60 * (12 - row + 1)
+
+    return input_img[y1:y2, x1:x2]
+
+def collectAvgBGR(image, positions, player, target_list):
+    for pos in positions:
+        row = pos[0]
+        col = pos[1]
+        puyo = cropPuyo(image, player, (row, col))
+        avg_data = np.average(np.average(puyo, axis=0), axis=0)
+        target_list.append(avg_data)
+
+red_BGR_data = []
+green_BGR_data = []
+blue_BGR_data = []
+yellow_BGR_data = []
+purple_BGR_data = []
+ojama_BGR_data = []
+
+# Rulue, blue, red, green, purple
+image = cv2.imread('test.png')
+blue_puyos = [(1, 1), (1, 2), (1, 3), (1, 5), (1, 6),
+              (2, 4), (3, 2), (3, 3), (3, 5), (3, 6),
+              (4, 3), (4, 6)]
+collectAvgBGR(image, blue_puyos, 1, blue_BGR_data)
+blue_puyos = [(1, 1), (1, 2), (1, 4), (2, 3), (3, 2), (3, 4), (3, 5),
+              (4, 4), (5, 2), (6, 1), (6, 2), (6, 5)]
+collectAvgBGR(image, blue_puyos, 2, blue_BGR_data)
+
+red_puyos = [(3, 1), (4, 1), (4, 2), (4, 4), (5, 3), (7, 1), (8, 1)]
+collectAvgBGR(image, red_puyos, 1, red_BGR_data)
+red_puyos = [(2, 1), (2, 2), (2, 5), (2, 6), (4, 5), (5, 1), (5, 5)]
+collectAvgBGR(image, red_puyos, 2, red_BGR_data)
+
+green_puyos = [(2, 1), (2, 2), (2, 3), (5, 6), (6, 4), (6, 5)]
+collectAvgBGR(image, green_puyos, 1, green_BGR_data)
+green_puyos = [(1, 3), (2, 4), (3, 3), (4, 3)]
+collectAvgBGR(image, green_puyos, 2, green_BGR_data)
+
+purple_puyos = [(1, 4), (2, 5), (2, 6), (3, 4), (4, 5), (5, 1), (5, 2), (5, 4), (5, 5),
+                (6, 1), (6, 3), (7, 4), (7, 5)]
+collectAvgBGR(image, purple_puyos, 1, purple_BGR_data)
+purple_puyos = [(1, 5), (1, 6), (3, 1), (3, 6), (4, 1), (4, 2), (5, 4), (6, 4), (8, 2)]
+collectAvgBGR(image, purple_puyos, 2, purple_BGR_data)
+
+ojama_puyos = [(4, 6), (5, 3), (7, 1), (7, 2), (7, 5)]
+collectAvgBGR(image, ojama_puyos, 2, ojama_BGR_data)
+
+
+image = cv2.imread('rulue_yellow.png')
+yellow_puyos = [(1, 4), (2, 4), (3, 3), (4, 3), (4, 6), (5, 6), (6, 4), (6, 6), (7, 5), (8, 4)]
+collectAvgBGR(image, yellow_puyos, 1, yellow_BGR_data)
+yellow_puyos = [(1, 5), (4, 1), (4, 6), (5, 2), (5, 6), (6, 2), (6, 6), (7, 4), (8, 1)]
+collectAvgBGR(image, yellow_puyos, 2, yellow_BGR_data)
+
+
+red_avg_triplet = np.average(np.array(red_BGR_data), axis=0)
+red_min_triplet = np.min(np.array(red_BGR_data), axis=0)
+red_max_triplet = np.max(np.array(red_BGR_data), axis=0)
+print('Red:')
+print(red_avg_triplet)
+print(red_min_triplet)
+print(red_max_triplet)
+print('\n\n')
+
+green_avg_triplet = np.average(np.array(green_BGR_data), axis=0)
+green_min_triplet = np.min(np.array(green_BGR_data), axis=0)
+green_max_triplet = np.max(np.array(green_BGR_data), axis=0)
+print('green:')
+print(green_avg_triplet)
+print(green_min_triplet)
+print(green_max_triplet)
+print('\n\n')
+
+blue_avg_triplet = np.average(np.array(blue_BGR_data), axis=0)
+blue_min_triplet = np.min(np.array(blue_BGR_data), axis=0)
+blue_max_triplet = np.max(np.array(blue_BGR_data), axis=0)
+print('blue:')
+print(blue_avg_triplet)
+print(blue_min_triplet)
+print(blue_max_triplet)
+print('\n\n')
+
+yellow_avg_triplet = np.average(np.array(yellow_BGR_data), axis=0)
+yellow_min_triplet = np.min(np.array(yellow_BGR_data), axis=0)
+yellow_max_triplet = np.max(np.array(yellow_BGR_data), axis=0)
+print('yellow:')
+print(yellow_avg_triplet)
+print(yellow_min_triplet)
+print(yellow_max_triplet)
+print('\n\n')
+
+purple_avg_triplet = np.average(np.array(purple_BGR_data), axis=0)
+purple_min_triplet = np.min(np.array(purple_BGR_data), axis=0)
+purple_max_triplet = np.max(np.array(purple_BGR_data), axis=0)
+print('purple:')
+print(purple_avg_triplet)
+print(purple_min_triplet)
+print(purple_max_triplet)
+print('\n\n')
+
+ojama_avg_triplet = np.average(np.array(ojama_BGR_data), axis=0)
+ojama_min_triplet = np.min(np.array(ojama_BGR_data), axis=0)
+ojama_max_triplet = np.max(np.array(ojama_BGR_data), axis=0)
+print('ojama:')
+print(ojama_avg_triplet)
+print(ojama_min_triplet)
+print(ojama_max_triplet)
+print('\n\n')
